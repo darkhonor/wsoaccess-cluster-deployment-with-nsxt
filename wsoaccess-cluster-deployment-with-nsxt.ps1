b44b0cdf82c36e0f8304892e10a5e0817a38b284
Alex Ackerman <aackerman@darkhonor.com>
2021-11-11 21:53:45 +0900

    Added support to deploy from Content Library.

--- wsoaccess-cluster-deployment-with-nsxt.ps1
+++ wsoaccess-cluster-deployment-with-nsxt.ps1
@@ -1,20 +1,29 @@
 # Author: Alex Ackerman
 # Website: www.vmhomelab.org
 
+#Requires -PSEdition Core
+#Requires -Modules VMware.PowerCLI
+
+
 # vCenter Server used to deploy the appliances
 $VIserver = "NEED VALUE"
 $VIUsername = "NEED VALUE"
 $VIPassword = "NEED VALUE"
 
 # OVA Source
-$wsoSourceOva = "C:\Users\aackerman\Downloads\identity-manager-20.10.0.0-17035009_OVF10.ova"
+# - If you will import a local OVA, fill in the full pathname for wsoSourceOva
+# - If you are using a Content Library Item, change useContentLibrary to $true and specify item name
+$wsoSourceOva = ""
+$useContentLibrary = $false
+$contentLibraryName = ""
+$wsoContentItemName = ""
 
-# Target Cluster and Datastore for the appliance.  
+# Target Cluster, Datastore, and VM Folder for the appliance.  
 $dsName = "Datastore Name"
 $clName = "Cluster Name"
+$folderName = "VM Folder Name"
 
-# To deploy a vApp, you have to specify the Host as well.  If the target host is the 
-# same, you can leave as is. Otherwise, uncomment for the other two appliance host targets.
+# To deploy a vApp, you have to specify the Host as well.  
 $hostName = "ESXi Hostname for the appliance"
 $vmName = "Appliance name in vCenter"
 
@@ -46,16 +55,6 @@ if ( "Core" -ne $PSVersionTable.PSEdition)
 
 # Verify that PowerCLI is installed and available
 
-
-# Verify Source OVA exists
-if ( !(Test-Path $wsoSourceOva)) 
-{
-    Write-Host -ForegroundColor Red "***Missing Appliance OVA.  Verify script matches actual file location"
-    exit
-} else {
-    Write-Host -ForegroundColor Yellow "* Appliance OVA Found: $wsoSourceOva"
-}
-
 # Validate key default values changed
 if ( "NEED VALUE" -eq $VIserver -or
          "NEED VALUE" -eq $VIUsername -or
@@ -64,6 +63,11 @@ if ( "NEED VALUE" -eq $VIserver -or
     Write-Host -ForegroundColor Red "***Default Values Set.  Unable to Proceed.  Edit Script before continuing***"
     exit
 }
+
+# You may want to uncomment this if you are having issues connecting to your vcenter server. WARNING: You MUST 
+# trust the certificate of the vCenter you are connecting to, because this will cause this script to ignore 
+# any errors
+#Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -DefaultVIServerMode Single -Scope:User
 
 # Log into the vCenter Server
 Write-Host -ForegroundColor Yellow "Connecting to vCenter..."
@@ -76,26 +80,32 @@ if ( $null -eq $vcConn )
 }
 Write-Host -ForegroundColor Yellow "Connected to vCenter"
 
-# Configure the vApp Properties
-Write-Host -ForegroundColor Yellow "Configuring vApp Settings..."
-$OVFConfig = Get-OvfConfiguration -Ovf $wsoSourceOva
-$OVFConfig.Common.vamitimezone.Value = $wsoTZ
-$OVFConfig.Common.ceip.enable.Value = $wsoCeip
-$OVFConfig.Common.vami.hostname.Value = $wsoHostname
-$OVFConfig.DeploymentOption.Value = $wsoDepOption
-$OVFConfig.IpAssignment.IpProtocol.Value = $wsoIpProtocol
-$OVFConfig.NetworkMapping.Network_1.Value = $wsoPortGroup
-$OVFConfig.vami.WorkspaceOneAccess.gateway.Value = $wsoGatewayIp
-$OVFConfig.vami.WorkspaceOneAccess.domain.Value = $wsoDomain
-$OVFConfig.vami.WorkspaceOneAccess.searchpath.Value = $wsoDomain
-$OVFConfig.vami.WorkspaceOneAccess.DNS.Value = $wsoDnsIp
-$OVFConfig.vami.WorkspaceOneAccess.ip0.Value = $wsoIp
-$OVFConfig.vami.WorkspaceOneAccess.netmask0.Value = $wsoNetmask
+# Verify Source 
+if ( !($useContentLibrary))
+{
+    if ( !(Test-Path $wsoSourceOva)) 
+    {
+        Write-Host -ForegroundColor Red "***Missing Appliance OVA.  Verify script matches actual file location"
+        exit
+    } else {
+        Write-Host -ForegroundColor Yellow "* Appliance OVA Found: $wsoSourceOva"
+    }
+} else {
+    $wsoContentItem = Get-ContentLibraryItem -Name $wsoContentItemName -ContentLibrary $contentLibraryName -Server $vcConn
+    if ( $null -eq $wsoContentItem )
+    {
+        Write-Host -ForegroundColor Red "***Missing Appliance OVA.  Verify script matches actual Content Library location"
+        exit
+    } else {
+        Write-Host -ForegroundColor Yellow "* Appliance OVA Found: $wsoContentItem.Name"
+    }
+}
 
 # Get the correct deployment location information
 $vmhost = Get-VMHost -Name $hostName
 $cluster = Get-Cluster -Name $clName
 $ds = Get-Datastore -Name $dsName
+$vmFolder = Get-Folder -Name $folderName
 
 # Validate the ESXi Host, Cluster, and Datastore for the Appliance
 if ( $null -eq $vmhost )
@@ -112,11 +122,42 @@ if ( $null -eq $ds )
 {
     Write-Host -ForegroundColor Red "***Datastore Not Found in vCenter***"
 }
+if ( $null -eq $vmFolder )
+{
+    Write-Host -ForegroundColor Red "***VM Folder Not Found in vCenter***"
+}
+
+# Configure the vApp Properties
+Write-Host -ForegroundColor Yellow "Configuring vApp Settings..."
+if ( !($useContentLibrary))
+{
+    $OVFConfig = Get-OvfConfiguration -Ovf $wsoSourceOva
+} else {
+    $OVFConfig = Get-OvfConfiguration -ContentLibraryItem $wsoContentItem -Target $vmhost
+}
+
+$OVFConfig.Common.vamitimezone.Value = $wsoTZ
+$OVFConfig.Common.ceip.enabled.Value = $wsoCeip
+$OVFConfig.Common.vami.hostname.Value = $wsoHostname
+$OVFConfig.DeploymentOption.Value = $wsoDepOption
+$OVFConfig.IpAssignment.IpProtocol.Value = $wsoIpProtocol
+$OVFConfig.NetworkMapping.Network_1.Value = $wsoPortGroup
+$OVFConfig.vami.WorkspaceOneAccess.gateway.Value = $wsoGatewayIp
+$OVFConfig.vami.WorkspaceOneAccess.domain.Value = $wsoDomain
+$OVFConfig.vami.WorkspaceOneAccess.searchpath.Value = $wsoDomain
+$OVFConfig.vami.WorkspaceOneAccess.DNS.Value = $wsoDnsIp
+$OVFConfig.vami.WorkspaceOneAccess.ip0.Value = $wsoIp
+$OVFConfig.vami.WorkspaceOneAccess.netmask0.Value = $wsoNetmask
 
 # Import the first appliance into the vCenter.
-Write-Host -ForegroundColor Yellow "Importing Primary Appliance..."
-$wsoVm = Import-VApp -Source $wsoSourceOva -OvfConfiguration $OVFConfig -Name $vmName -VMHost $vmhost -Location $cluster -Datastore $ds -DiskStorageFormat thin -Confirm:$false
-Write-Host -ForegroundColor Yellow "Primary Appliance Imported to vCenter"
+Write-Host -ForegroundColor Yellow "Create Primary Appliance..."
+if ($useContentLibrary)
+{
+    $wsoVm = New-VM -ContentLibraryItem $wsoContentItem -OvfConfiguration $OVFConfig -Name $vmName -VMHost $vmhost -Location $vmFolder -Datastore $ds -DiskStorageFormat Thin -Confirm:$false
+} else {
+    $wsoVm = Import-VApp -Source $wsoSourceOva -OvfConfiguration $OVFConfig -Name $vmName -VMHost $vmhost -Location $cluster -Datastore $ds -DiskStorageFormat thin -Confirm:$false
+}
+Write-Host -ForegroundColor Yellow "Primary Appliance Created"
 
 Write-Host -ForegroundColor Yellow "Powering on $vmName"
 $wsoVm | Start-VM -RunAsync | Out-Null
